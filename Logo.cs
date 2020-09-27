5898b6561fc8652e720d91871fcf3aa09da501bb
Владимир <51334865+wingofnight@users.noreply.github.com>
2020-09-27 20:25:34 +0300

    Add files via upload

---
+++ Logo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PAIN2;
+using PaintAndPain2077;
+using static System.Console;
+namespace GrayLive
+{ 
+   
+    class Logo:Paint
+    {
+        public void print()
+        { 
+            Write("\n\n");
+            Gray(27);
+            WriteLine("");
+            Gray(2); Red();Black(4);Gray();Red();Black(4);Gray();Red();Black(4);Red();Black();Gray(2);Red();Black(); Gray(2);
+            WriteLine("");
+            Gray(2); Red();Black();Gray(4);Red();Black();Gray();Red();Black();Gray();Red();Black();Gray();Red();Black();Gray();Red();Black();Red();Black();Gray(3);
+            WriteLine("");
+            Gray(2); Red();Black();Red();Black(2);Gray();Red();Black(4);Gray();Red();Black(4);Gray(2);Red();Black();Gray(4);
+            WriteLine("");
+            Gray(2);Red();Black();Gray();Red();Black();Gray();Red();Black(2);Gray(3);Red();Black();Gray();Red();Black();Gray(2);Red();Black();Gray(4);
+            WriteLine("");
+            Gray(2);Red();Black(4);Gray();Red();Black();Red();Black(2);Gray();Red();Black();Gray();Red();Black();Gray(2);Red();Black();Gray(4);
+            WriteLine("");
+            Gray(27);
+            WriteLine("");
+            Gray(6);Red();Black();Gray(3);Red();Black();Red();Black();Gray(2);Red();Black();Red();Black(3);Gray(4);
+            WriteLine("");
+            Gray(6); Red(); Black(); Gray(5);Red();Black();Gray(2);Red();Black();Red();Black();Gray(6);
+            WriteLine("");
+            Gray(6); Red(); Black(); Gray(3); Red(); Black(); Red(); Black(); Gray(2); Red(); Black(); Red(); Black(2); Gray(5);
+            WriteLine("");
+            Gray(6); Red(); Black();Gray();Red();Black();Red();Black();Gray();Red();Black();Red();Black();Gray();Red();Black();Gray(6);
+            WriteLine("");
+            Gray(6); Red(); Black(4);Red();Black();Gray(2);Red();Black();Gray(2);Red();Black(5);Gray(2);
+            WriteLine("");
+            Gray(27);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using static System.Console;
+
+namespace GrayLive
+{
+    class Program
+    {             
+        static void Main(string[] args)
+        {
+            string puth;
+             Bitmap bmp;
+            Logo logo = new Logo();
+            
+            do
+            {
+                Clear();
+                logo.print();
+                WriteLine("\n\nHello user\n\nIf you wont more gray in you live press ENTER");
+                ConsoleKeyInfo getch = ReadKey(true);
+                if (getch.Key == ConsoleKey.Enter)
+                {
+                    while (true)
+                    {
+                        Clear();
+                        logo.print();
+                        WriteLine("\n\nOK, just enter the puth to file end press \"ENTER\"\n\n");
+                        ForegroundColor = ConsoleColor.DarkYellow;
+                        puth = ReadLine();
+                        ResetColor();
+                        LoadPicture(puth);
+                    }                    
+                }
+                else
+                {
+                    ForegroundColor = ConsoleColor.DarkRed;
+                    WriteLine("\n\nIT IS NOT A ENTER!!! PRESS ENTER, YOU!!!");
+                    ResetColor();
+                    ReadKey();
+                }
+            } while (true);                  
+           
+            void LoadPicture(string filename)
+            {
+                
+                try
+                {
+                    bmp = new Bitmap(Image.FromFile(filename));
+                    ChangePicture();
+                    ForegroundColor = ConsoleColor.DarkGreen;
+                    WriteLine("\n\nPicture Change!");
+                    ResetColor();
+                    ReadKey();
+                }
+                catch (Exception)
+                {
+                    ForegroundColor = ConsoleColor.DarkRed;
+                    WriteLine("\n\nAdress is not foud 404");
+                    ResetColor();
+                    ReadKey();
+                }
+            }
+            void ChangePicture()
+            {
+                int size = puth.Length - 4;
+                string F_name;
+                F_name = puth;
+                F_name = F_name.Remove(size, 4);
+                Bitmap res = new Bitmap(bmp);
+                for (int y = 0; y < bmp.Height; y++)
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        Color pixel = bmp.GetPixel(x, y);
+
+                        pixel = ChangeGrayscale(pixel);
+
+                        res.SetPixel(x, y, pixel);
+                    }
+                res.Save(F_name + "-result.jpg");
+            }
+            Color ChangeGrayscale(Color pixel)
+            {
+                int avg = (pixel.R + pixel.G + pixel.B + 1) / 3;
+                return Color.FromArgb(avg, avg, avg);
+            }          
+
+        }
+
+    }
+   
+}
